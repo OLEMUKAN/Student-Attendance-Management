957115b6d5f7d5db80405802be794425a877a3a1
OLEMUKAN <olemukan4@gmail.com>
2024-09-04 21:27:19 +0300

    chore: Update .gitignore to include .env file

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -7,12 +7,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCP4HGQo9E5HsKU724znuPq2efApQX42A0",
-  authDomain: "attendance-b5029.firebaseapp.com",
-  projectId: "attendance-b5029",
-  storageBucket: "attendance-b5029.appspot.com",
-  messagingSenderId: "396880833878",
-  appId: "1:396880833878:web:0fb40ddc2612a65e477af7"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
